9c10a9c1f4c070d99f781e51034a9c54c3d2c51c
hsjung22 <hsjung22@gmail.com>
2017-07-12 19:43:18 -0700

    fix: ListBooks should handle its own state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react'
-import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import './App.css'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 
 class BooksApp extends Component {
-  state = {
-    books: []
-  }
-
-  componentDidMount() {
-    BooksAPI.getAll().then(books =>
-      this.setState({ books })
-    )
-  }
-
-  handleShelfUpdate = (book, shelf) => {
-    this.setState(prevState => ({
-      books: prevState.books.map(b =>
-        (b.id === book.id)
-          ? {...b, shelf}
-          : b
-      )
-    }))
-
-    BooksAPI.update(book, shelf)
-  }
-
   render() {
-    const { books } = this.state
-    const { handleShelfUpdate } = this
     return (
       <div className="app">
         <Route
@@ -40,12 +15,7 @@ class BooksApp extends Component {
         <Route
           exact
           path="/"
-          render={() =>
-            <ListBooks
-              books={books}
-              handleShelfUpdate={handleShelfUpdate}
-            />
-          }
+          component={ListBooks}
         />
       </div>
     )
