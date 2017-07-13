27b3e239145080e53c6c23848dcffad29406ad6f
hsjung22 <hsjung22@gmail.com>
2017-07-12 20:49:12 -0700

    refactor: App no longer has state, use normal functions instead of class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
 import './App.css'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 
-class BooksApp extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Route
-          path="/search"
-          component={SearchBooks}
-        />
-        <Route
-          exact
-          path="/"
-          component={ListBooks}
-        />
-      </div>
-    )
-  }
+function BooksApp() {
+  return (
+    <div className="app">
+      <Route
+        path="/search"
+        component={SearchBooks}
+      />
+      <Route
+        exact
+        path="/"
+        component={ListBooks}
+      />
+    </div>
+  )
 }
 
 export default BooksApp
